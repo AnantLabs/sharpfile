073df3567a778dcd0c06a7798d606bfd6c458d3e
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-02-25 03:40:49 +0000

    Delete old projects since they were merged together. Issue: 56.

--- SharpFile/SharpFile.IO/Retrievers/ChildResourceRetriever.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using SharpFile.Infrastructure;
-
-namespace SharpFile.IO.Retrievers {
-    public abstract class ChildResourceRetriever {
-        public delegate void GetCompleteDelegate();
-        public delegate bool CustomMethodDelegate(IResource resource);
-    }
-}
