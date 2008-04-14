f57e13dde370c52b3082b992be322c64121dc313
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-04-14 04:24:02 +0000

    Validate changes user makes when renaming a file. Show a balloon tip if the character is invalid. Issue 73.

--- Infrastructure/Shell32.cs
+++ Infrastructure/Shell32.cs
@@ -24,10 +24,60 @@ namespace SharpFile.Infrastructure {
         public const int LVM_FIRST = 0x1000;
         public const int LVM_GETEDITCONTROL = (LVM_FIRST + 24);
 
+        //public const int ECM_FIRST = 0x1500;
+        //public const int EM_SHOWBALLOONTIP = ECM_FIRST + 3;
+        //public const int EM_LIMITTEXT = 0xC5;
+        //public const int EM_SETSEL = 0x00B1;
+
+        /*
+ * Edit Control Messages
+ */
+        public const int EM_GETSEL = 0x00B0;
+        public const int EM_SETSEL = 0x00B1;
+        public const int EM_GETRECT = 0x00B2;
+        public const int EM_SETRECT = 0x00B3;
+        public const int EM_SETRECTNP = 0x00B4;
+        public const int EM_SCROLL = 0x00B5;
+        public const int EM_LINESCROLL = 0x00B6;
+        public const int EM_SCROLLCARET = 0x00B7;
+        public const int EM_GETMODIFY = 0x00B8;
+        public const int EM_SETMODIFY = 0x00B9;
+        public const int EM_GETLINECOUNT = 0x00BA;
+        public const int EM_LINEINDEX = 0x00BB;
+        public const int EM_SETHANDLE = 0x00BC;
+        public const int EM_GETHANDLE = 0x00BD;
+        public const int EM_GETTHUMB = 0x00BE;
+        public const int EM_LINELENGTH = 0x00C1;
+        public const int EM_REPLACESEL = 0x00C2;
+        public const int EM_GETLINE = 0x00C4;
+        public const int EM_LIMITTEXT = 0x00C5;
+        public const int EM_CANUNDO = 0x00C6;
+        public const int EM_UNDO = 0x00C7;
+        public const int EM_FMTLINES = 0x00C8;
+        public const int EM_LINEFROMCHAR = 0x00C9;
+        public const int EM_SETTABSTOPS = 0x00CB;
+        public const int EM_SETPASSWORDCHAR = 0x00CC;
+        public const int EM_EMPTYUNDOBUFFER = 0x00CD;
+        public const int EM_GETFIRSTVISIBLELINE = 0x00CE;
+        public const int EM_SETREADONLY = 0x00CF;
+        public const int EM_SETWORDBREAKPROC = 0x00D0;
+        public const int EM_GETWORDBREAKPROC = 0x00D1;
+        public const int EM_GETPASSWORDCHAR = 0x00D2;
+
+        public const int EM_SETMARGINS = 0x00D3;
+        public const int EM_GETMARGINS = 0x00D4;
+        public const int EM_SETLIMITTEXT = EM_LIMITTEXT;   /* ;win= 40; Name change */
+        public const int EM_GETLIMITTEXT = 0x00D5;
+        public const int EM_POSFROMCHAR = 0x00D6;
+        public const int EM_CHARFROMPOS = 0x00D7;
+
+        public const int EM_SETIMESTATUS = 0x00D8;
+        public const int EM_GETIMESTATUS = 0x00D9;
+
         public const int ECM_FIRST = 0x1500;
         public const int EM_SHOWBALLOONTIP = ECM_FIRST + 3;
-        public const int EM_LIMITTEXT = 0xC5;
-        public const int EM_SETSEL = 0x00B1;
+
+        public const int ENM_CHANGE = 0x0300;
 
         public static int cbFileInfo = Marshal.SizeOf(typeof(SHFILEINFO));
         public static int cbMenuItemInfo = Marshal.SizeOf(typeof(MENUITEMINFO));
@@ -39,6 +89,8 @@ namespace SharpFile.Infrastructure {
         #region DLL Import
 
         #region Shell32
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(int hWnd, StringBuilder text, int count);
 
         // Retrieves information about an object in the file system,
         // such as a file, a folder, a directory, or a drive root.
@@ -172,6 +224,27 @@ namespace SharpFile.Infrastructure {
         #endregion
 
         #region User32
+        [DllImport("User32", SetLastError = true)]
+        private static extern bool SetWindowPos(
+            IntPtr hWnd,
+            IntPtr hWndInsertAfter,
+            int X,
+            int Y,
+            int cx,
+            int cy,
+            int uFlags);
+
+        [DllImport("User32", SetLastError = true)]
+        private static extern int GetClientRect(
+            IntPtr hWnd,
+            ref Rectangle lpRect);
+
+        //[DllImport("User32", SetLastError = true)]
+        //private static extern int SendMessage(
+        //    IntPtr hWnd,
+        //    int Msg,
+        //    int wParam,
+        //    IntPtr lParam);
 
         // Sends the specified message to a window or windows
         [DllImport("user32",
