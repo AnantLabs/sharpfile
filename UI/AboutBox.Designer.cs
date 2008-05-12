cf42b2410dfe9aae99cb696409eeb5c7f6313921
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-05-12 02:42:09 +0000

    Add in About box. Issue 83.

---
+++ UI/AboutBox.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SharpFile.UI {
+    partial class AboutBox {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.okButton = new System.Windows.Forms.Button();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.labelCompanyName = new System.Windows.Forms.Label();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.labelProductName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.okButton.Location = new System.Drawing.Point(181, 116);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 4;
+            this.okButton.Text = "&OK";
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.Location = new System.Drawing.Point(12, 80);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(244, 23);
+            this.linkLabel.TabIndex = 3;
+            // 
+            // labelCompanyName
+            // 
+            this.labelCompanyName.Location = new System.Drawing.Point(12, 55);
+            this.labelCompanyName.Name = "labelCompanyName";
+            this.labelCompanyName.Size = new System.Drawing.Size(244, 23);
+            this.labelCompanyName.TabIndex = 2;
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.Location = new System.Drawing.Point(12, 32);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(244, 23);
+            this.labelVersion.TabIndex = 1;
+            // 
+            // labelProductName
+            // 
+            this.labelProductName.Location = new System.Drawing.Point(12, 9);
+            this.labelProductName.Name = "labelProductName";
+            this.labelProductName.Size = new System.Drawing.Size(244, 23);
+            this.labelProductName.TabIndex = 0;
+            // 
+            // AboutBox
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 151);
+            this.Controls.Add(this.labelProductName);
+            this.Controls.Add(this.labelVersion);
+            this.Controls.Add(this.labelCompanyName);
+            this.Controls.Add(this.linkLabel);
+            this.Controls.Add(this.okButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutBox";
+            this.Padding = new System.Windows.Forms.Padding(9);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.Label labelCompanyName;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Label labelProductName;
+    }
+}
