4222989358a28bfcbe9100c450938f50c91568ae
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-05-12 02:42:09 +0000

    Add in About box. Issue 83.

---
+++ trunk/UI/AboutBox.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace SharpFile.UI {
+    partial class AboutBox : Form {
+        private string website = "http://sharpfile.googlecode.com";
+
+        public AboutBox() {
+            InitializeComponent();
+            this.Text = String.Format("About {0}", 
+                AssemblyTitle);
+            this.labelProductName.Text = AssemblyProduct;
+            this.labelVersion.Text = string.Format("Version {0}", 
+                AssemblyVersion);            
+            this.labelCompanyName.Text = AssemblyCompany;
+            this.linkLabel.Text = website;            
+            //this.textBoxDescription.Text = AssemblyDescription;
+
+            this.linkLabel.Click += delegate {
+                Process.Start(website);
+            };
+
+            this.okButton.Click += delegate {
+                this.Close();
+            };
+        }
+
+        #region Assembly Attribute Accessors
+
+        public string AssemblyTitle {
+            get {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0) {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "") {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        public string AssemblyVersion {
+            get {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string AssemblyDescription {
+            get {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                if (attributes.Length == 0) {
+                    return "";
+                }
+                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
+        public string AssemblyProduct {
+            get {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes.Length == 0) {
+                    return "";
+                }
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        public string AssemblyCompany {
+            get {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                if (attributes.Length == 0) {
+                    return "";
+                }
+                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+        #endregion
+    }
+}
