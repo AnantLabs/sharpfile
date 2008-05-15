57bb8207ad4ade6858aa3286688a679f6e4dd1fe
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-05-15 06:08:08 +0000

    Make sure that compressed directory's names are correct. Only show compressed files and no compressed directories in a compressed file.

--- IO/ChildResources/CompressedDirectoryInfo.cs
+++ IO/ChildResources/CompressedDirectoryInfo.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.IO;
-using SharpFile.Infrastructure;
 
 namespace SharpFile.IO.ChildResources {
     public class CompressedDirectoryInfo : DirectoryInfo {
         public CompressedDirectoryInfo(string fullName, string name, DateTime lastWriteTime)
             : base(name, fullName, name, FileAttributes.Normal, 0, DateTime.MinValue, DateTime.MinValue,
             lastWriteTime, null) {
+            this.name = name;
         }
     }
 }
