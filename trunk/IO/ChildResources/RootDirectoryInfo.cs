e5f2868d55f956a1427040f9a82f1f85d9086de5
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-06-10 01:39:26 +0000

    Do not show any datetime information for root directories.

--- trunk/IO/ChildResources/RootDirectoryInfo.cs
+++ trunk/IO/ChildResources/RootDirectoryInfo.cs
@@ -1,13 +1,31 @@
 using SharpFile.IO.ParentResources;
 
 namespace SharpFile.IO.ChildResources {
-	public class RootDirectoryInfo : DriveInfo {
+    public class RootDirectoryInfo : DriveInfo {
         private const string rootDirectoryName = ".";
 
         public RootDirectoryInfo(string fullName)
             : base(fullName) {
             name = Path;
             displayName = rootDirectoryName;
-		}
+        }
+
+        public string LastWriteTime {
+            get {
+                return string.Empty;
+            }
+        }
+
+        public string CreationTime {
+            get {
+                return string.Empty;
+            }
+        }
+
+        public string LastAccessTime {
+            get {
+                return string.Empty;
+            }
+        }
     }
 }
