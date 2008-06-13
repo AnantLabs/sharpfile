fc933d24591104e0663b9d145bcecd43e1b225a6
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-06-13 03:28:21 +0000

    Update changelog and assembly info to 0.7.0.061208.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.7.0.052008")]
-[assembly: AssemblyFileVersion("0.7.0.052008")]
+[assembly: AssemblyVersion("0.7.0.061208")]
+[assembly: AssemblyFileVersion("0.7.0.061208")]
