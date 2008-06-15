3327bce6aa75bbc17e47365448d4d6484e178653
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-06-15 20:17:45 +0000

    Add ability to customize font of listview.

---
+++ trunk/Infrastructure/SettingsSection/FontInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace SharpFile.Infrastructure.SettingsSection {
+    public class FontInfo {
+        private string familyName;
+        private float size;
+        private FontStyle style;
+
+        public FontInfo() {
+        }
+
+        public FontInfo(string familyName, float size, FontStyle style) {
+            this.familyName = familyName;
+            this.size = size;
+            this.style = style;
+        }
+
+        public string FamilyName {
+            get {
+                return familyName;
+            }
+            set {
+                familyName = value;
+            }
+        }
+
+        public float Size {
+            get {
+                return size;
+            }
+            set {
+                size = value;
+            }
+        }
+
+        public FontStyle Style {
+            get {
+                return style;
+            }
+            set {
+                style = value;
+            }
+        }
+    }
+}
