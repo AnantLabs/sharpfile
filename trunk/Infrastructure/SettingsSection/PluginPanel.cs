5a3669980e3c30b3094eb68762d0b80669f4a98e
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-06-30 01:42:15 +0000

    Rename IPanel to IPluginPanel. Store plugin panel settings in the config file.

---
+++ trunk/Infrastructure/SettingsSection/PluginPanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Serialization;
+
+namespace SharpFile.Infrastructure.SettingsSection {
+    public class PluginPanel {
+        string name;
+        FullyQualifiedType type;
+        FullyQualifiedType settingsType;
+
+        public PluginPanel() {
+        }
+
+        public PluginPanel(string name, FullyQualifiedType type, FullyQualifiedType settingsType) {
+            this.name = name;
+            this.type = type;
+            this.settingsType = settingsType;
+        }
+
+        [XmlAttribute]
+        public string Name {
+            get {
+                return name;
+            }
+            set {
+                name = value;
+            }
+        }
+
+        public FullyQualifiedType Type {
+            get {
+                return type;
+            }
+            set {
+                type = value;
+            }
+        }
+
+        public FullyQualifiedType SettingsType {
+            get {
+                return settingsType;
+            }
+            set {
+                settingsType = value;
+            }
+        }
+    }
+}
