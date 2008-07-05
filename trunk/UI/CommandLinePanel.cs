646746e27372e9f784967b82c1188d572d154de8
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-07-05 03:46:42 +0000

    Add command line panel. Delete FakeListView.

--- trunk/UI/CommandLinePanel.cs
+++ trunk/UI/CommandLinePanel.cs
@@ -1,10 +1,14 @@
 ﻿using System.Windows.Forms;
 using SharpFile.Infrastructure;
+using WeifenLuo.WinFormsUI.Docking;
 
 namespace SharpFile.UI {
-    public partial class CommandLinePanel : UserControl, IPluginPanel {
+    public partial class CommandLinePanel : DockContent, IPluginPanel {
         public CommandLinePanel() {
             InitializeComponent();
+            this.TabText = "Command Line";
+            //this.DockAreas = DockAreas.DockTop | DockAreas.DockTop | DockAreas.Document;
+            this.AllowEndUserDocking = false;
 
             this.SizeChanged += delegate {
                 this.txtResults.Width = this.Width - 10;
