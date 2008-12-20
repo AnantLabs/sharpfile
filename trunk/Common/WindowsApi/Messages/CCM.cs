fa13a1d4547b1747bcd42d8dd792fec8704f90ea
longueur.software <longueur.software@93d6a80d-e03f-0410-b03d-db30a65fbe19>
2008-12-20 16:34:56 +0000

    Delete WindowsApi folder.

--- trunk/Common/WindowsApi/Messages/CCM.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Common.WindowsApi.Messages {
-    public enum CCM : int {
-        FIRST = 0x2000,// Common control shared messages
-        SETUNICODEFORMAT = (FIRST + 5),
-        GETUNICODEFORMAT = (FIRST + 6)
-    }
-}
